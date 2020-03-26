13c12ff0d831044058c67b555110caea340b91ba
hhalkola <herman.halkola@gmail.com>
2020-03-26 18:47:17 +0200

    //

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#app',
     data: {
+    //Array of characters
     characters: [
         {
             char_id: '',
@@ -9,23 +10,29 @@ var app = new Vue({
             img: '',
             nickname: '',
             status: '',
+            portrayed:'',
             occupation: []
         }
     ],
+    //Helper to be used with combobox
     selectedIndex: 0
     },
+    //Show data when loaded
     mounted: function(){
         this.get_characters()
     },
     methods: {
+        //Api call
         get_characters: function(){
             axios.get('https://www.breakingbadapi.com/api/characters/')
             .then(function (response){
+                //For dev purpose console.log to see what you get from url
                 console.log(response)
+            //Read data to characters
             this.app.characters = response.data;
-            console.log(response.data)
             })
         },
+        //Helper function to be called when selection changes in combobox
         switchView: function(event, selectedIndex) {
         console.log(event, selectedIndex);
         this.selectedIndex = selectedIndex;
